292a1c5012eddd1f3db4646fd16241013322d206
Rakesh07804 <lolururakesh@gmail.com>
2026-04-19 15:38:27 +0530

    raiqa assignment completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const meals = [
+    { id: 1, name: "Biryani", price: 150, isAvailable: true },
+    { id: 2, name: "Noodles", price: 100, isAvailable: false },
+    { id: 3, name: "Paneer Curry", price: 180, isAvailable: true },
+  ];
+
+  const [selectedMeals, setSelectedMeals] = useState([]);
+  const [showAvailable, setShowAvailable] = useState(true);
+  const [sortOrder, setSortOrder] = useState("low");
+  const filteredMeals = showAvailable
+      ? meals.filter((m) => m.isAvailable)
+      : meals;
+
+  const sortedMeals = [...filteredMeals].sort((a, b) =>
+  sortOrder === "low" ? a.price - b.price : b.price - a.price
+  );
+  
+
+  const addMeal = (meal) => {
+    const alreadyAdded = selectedMeals.find((m) => m.id === meal.id);
+
+    if (!alreadyAdded) {
+    setSelectedMeals([...selectedMeals, meal]);
+    }
+  };
+const total = selectedMeals.reduce((sum, meal) => sum + meal.price, 0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meal App</h1>
+
+      <h2>Meals List</h2>
+
+      {sortedMeals.map((meal) => (
+        <div key={meal.id}>
+          {meal.name} - ₹{meal.price}
+          <button onClick={() => addMeal(meal)}> Add </button>
+        </div>
+      ))}
+
+      <h2>Selected Meals</h2>
+
+      {selectedMeals.map((meal) => (
+        <div key={meal.id}>
+          {meal.name} - ₹{meal.price}
+        </div>
+      ))}
+
+      <h3>Total: ₹{total}</h3>
     </div>
   );
 }
